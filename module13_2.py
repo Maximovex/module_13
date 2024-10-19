9b52b54bef742088c75cc216137c202e5a8cc540
Maximovex <maximove81@gmail.com>
2024-10-19 17:41:53 +0300

    Update module13_2.py

--- module13_2.py
+++ module13_2.py
@@ -2,16 +2,10 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import asyncio
 
-api = '7780851910:AAFlpxho6e15lfa52eedHI8YiENQlLrQKQQ'
+api = ''
 
 bot = Bot(token=api)
 dp = Dispatcher()
-
-# @dp.message(Command("start"))
-# async def cmd_start(message: types.Message):
-#     await message.answer("Hello!")
-#     print('Привет! Я бот помогающий твоему здоровью.')
-
 
 @dp.message(Command('start'))
 async def start(message):
